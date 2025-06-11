e31f30489b7d7776122a17addb5032116a96faf8
Antonio Duran Sabates <antonio.d@students.salle.url.edu>
2025-06-11 13:10:54 +0200

    Gurdar notes incial

--- main.asm
+++ main.asm
@@ -1,259 +1,297 @@
 #include <p18f4321.inc>
 
-    CONFIG OSC = INTIO2
-    CONFIG WDT = OFF
-    CONFIG PBADEN = DIG
-    CONFIG MCLRE = ON       
+CONFIG  OSC=HSPLL      ; Oscilador de alta velocidad
+CONFIG  PBADEN=DIG  ; PORTB en modo digital
+CONFIG  WDT=OFF     ; Desactivar el Watchdog Timer
     
-
-
-; ----------------------------
-; DECLARACIÓN DE VARIABLES
-; ----------------------------
-TEMP         EQU 0x20  ; Variable para almacenar datos temporales
-TEMP_DELAY   EQU 0x21  ; Variable para los retardos
-NOTA_TEMP    EQU 0x22  ; Variable para almacenar la nota seleccionada
-DISTANCIA    EQU 0x23  
-TICS_COUNTER_L EQU 0x24
-
-R0 EQU 0x56
-R1 EQU 0x57
+    
+SEVENSEGMENTS_OFF EQU b'11111111'  ; Apagado
+SEVENSEGMENTS_GUION EQU b'11111101' ; 0: a, b, c, d, e, f
+SEVENSEGMENTS_CERO EQU b'10000010' ; 0: a, b, c, d, e, f
+SEVENSEGMENTS_UNO EQU b'11111010'  ; 1: b, c
+SEVENSEGMENTS_DOS EQU b'10010001'  ; 2: a, b, d, e, g
+SEVENSEGMENTS_TRES EQU b'10000101' ; 3: 
+SEVENSEGMENTS_CUATRO EQU b'11001100' ; 4: b, c, f, g
+SEVENSEGMENTS_CINCO EQU b'10100100'  ; 5: a, c, d, f, g
+SEVENSEGMENTS_SEIS EQU b'10100000'   ; 6: a, c, d, e, f, g
+SEVENSEGMENTS_SIETE EQU b'10001111'  ; 7: a, b, c
+SEVENSEGMENTS_OCHO EQU b'10000000'   ; 8: a, b, c, d, e, f, g
+SEVENSEGMENTS_NUEVE EQU b'10001100'  ; 9: a, b, c, d, f, g
  
-ORG 0x0000
-GOTO MAIN
-ORG 0x0008
-GOTO HIGH_RSI
-ORG 0x0018
-RETFIE FAST
+CANTIDAD_NOTAS EQU   0x0020
+PRIMER_DATO EQU      0x0030 ;A PARTIR DE AQUÍ COMENZAMOS A GUARDAR LAS NOTAS (del bit 0 al 2) Y LAS DURACIONES (del bit 3 al 4)
+PUNTERO_7SEG EQU     0x0040
+TEMP EQU   0x0050
+
+
+
+
+ORG     0x0000
+GOTO    MAIN
+
  
+ 
+; Subrutina para cargar el Timer0 (20 ms)
+CARREGA_TIMER
+    BCF     INTCON,TMR0IF,0   ; Limpiar el bit de interrupción
+    MOVLW   0xF3              ; Valor bajo para 20 ms
+    MOVWF   TMR0L             ; Escribir el valor en TMR0L
+    MOVLW   0xFC              ; Valor bajo para 20 ms
+    MOVWF   TMR0H             ; Escribir el valor en TMR0H
+    RETURN
+    
+ESPERA
+    ; Configurar y cargar el Timer0 para 20 ms
+    BSF T0CON, 7, 0     ; Activar el Timer0
+    CALL CARREGA_TIMER       ; Carga los valores iniciales para 20 ms
+    COMPROBAR
+    BTFSS INTCON, TMR0IF, 0  ; Verificar si se cumplió el periodo
+    GOTO COMPROBAR         ; Si no, seguir esperando
+
+    ; Desactivar el Timer0 y limpiar el flag de interrupción
+    BCF T0CON, TMR0ON, 0     ; Desactivar el Timer0
+    BCF INTCON, TMR0IF, 0    ; Limpiar el flag del Timer0
+    ;CLRF FSR0H
+    MOVF FSR0
+    ADDLW 0x01
+    MOVWF FSR0L
+    RETURN
+
+
 HIGH_RSI
-    BCF INTCON, GIE, ACCESS    
-    BCF INTCON, TMR0IE, ACCESS  
-    
-    BTFSS INTCON, TMR0IF, ACCESS    ;Mirem si la interrupcio es del timer0 sino la saltem
-    RETFIE FAST
-   
-    BCF INTCON, TMR0IF
-    MOVLW HIGH(.64736)	    
-    MOVWF TMR0H,0
-    MOVLW LOW(.64736)	   
-    MOVWF TMR0L,0
-    BSF LATA,4,0
-    BTFSC PORTB, 3		; Si echo actiu, incrementa comptador
-    CALL INCREMENT_COUNTER
-  
-    BSF INTCON, GIE, ACCESS     ; Habilitar interrupciones periféricas
-    BSF INTCON, TMR0IE, ACCESS   ; Habilitar interrupción del Timer0
-    RETFIE FAST
-    
-; --------------------------------------------
-; ENVÍA UN PULSO ULTRASÓNICO DE 10us
-; --------------------------------------------
-ENVIAR_PULSO_10US
-   
-    BSF LATB,2,0  ; Activar TRIGGER (RB2)
-    NOP
-    NOP
-    NOP
-    BCF LATB,2,0  ; Desactivar TRIGGER
-    CALL ESPERA_50MS
-    RETURN
-; --------------------------------------------
-; MIDE EL TIEMPO DEL PULSO ECHO
-; --------------------------------------------
-MEDIR_DISTANCIA
-    CLRF TICS_COUNTER_L
-
-ESPERAR_ECHO_ACTIVO
-    BTFSS PORTB,3
-    GOTO ESPERAR_ECHO_ACTIVO   ; Esperar hasta que ECHO = 1
-    
-ESPERAR_ECHO_BAJO
-    BTFSC PORTB,3
-    GOTO ESPERAR_ECHO_BAJO     ; Esperar mientras ECHO = 1
-    
-    MOVF TICS_COUNTER_L, W
-    MOVWF DISTANCIA
-    CLRF TICS_COUNTER_L
-    RETURN
-
-; --------------------------------------------
-; MAPEA DISTANCIA A UNA NOTA MUSICAL
-; --------------------------------------------
-CALCULAR_NOTA
-   
-    MOVLW .6
-    CPFSGT DISTANCIA
-    GOTO SET_DO
-
-    MOVLW .12
-    CPFSGT DISTANCIA
-    GOTO SET_RE
-
-    MOVLW .17
-    CPFSGT DISTANCIA
-    GOTO SET_MI
-
-    MOVLW .23
-    CPFSGT DISTANCIA
-    GOTO SET_FA
-
-    MOVLW .29
-    CPFSGT DISTANCIA
-    GOTO SET_SOL
-
-    MOVLW .35
-    CPFSGT DISTANCIA
-    GOTO SET_LA
-
-    GOTO SET_SI
-
-SET_DO
-    MOVLW b'00000001'
-    MOVWF LATD
-    MOVLW .0
-    GOTO SET_NOTA
-SET_RE
-    MOVLW b'00000010'
-    MOVWF LATD
-    MOVLW .2
-    GOTO SET_NOTA
-SET_MI
-    MOVLW b'00000011'
-    MOVWF LATD
-    MOVLW .4
-    GOTO SET_NOTA
-SET_FA
-    MOVLW b'00000100'
-    MOVWF LATD
-    MOVLW .6
-    GOTO SET_NOTA
-SET_SOL
-    MOVLW b'00000101'
-    MOVWF LATD
-    MOVLW .8
-    GOTO SET_NOTA
-SET_LA
-    MOVLW b'00000110'
-    MOVWF LATD
-    MOVLW .10
-    GOTO SET_NOTA
-SET_SI
-    MOVLW b'00000111'
-    MOVWF LATD
-    MOVLW .12
-
-SET_NOTA
-    MOVWF NOTA_TEMP
-    RETURN
-
-; --------------------------------------------
-; GENERA ONDA CUADRADA EN RC5 (ALTAVOZ)
-; --------------------------------------------
-GENERAR_SONIDO_RC5
-    MOVF NOTA_TEMP, W   ; Usamos la nota seleccionada
-    MOVWF TEMP          ; Pasamos el valor a TEMP
-LOOP_SONIDO
-    BSF LATC,5,0  ; Activar RC5 (Señal alta)
-    CALL RETARDO
-    BCF LATC,5,0  ; Apagar RC5 (Señal baja)
-    CALL RETARDO
-    DECFSZ TEMP,1 ; Reducimos TEMP y repetimos hasta que se acabe
-    GOTO LOOP_SONIDO
-    RETURN
-
-; --------------------------------------------
-; RETARDO CONTROLADO POR TEMP PARA FRECUENCIA
-; --------------------------------------------
-ESPERA_50MS
-    MOVLW   0x0A
-    MOVWF   R0
-
-OUTER_LOOP
-    MOVLW   0xFA
-    MOVWF   R1
-
-INNER_LOOP
-    NOP
-    NOP
-    DECFSZ  R1, F
-    GOTO    INNER_LOOP
-
-    DECFSZ  R0, F
-    GOTO    OUTER_LOOP
-
-    RETURN
+    BTFSS   INTCON,TMR0IF,0   ; Verificar si la interrupción es del Timer0
+    RETFIE  FAST		
+    CALL    CARREGA_TIMER     ; Recargar el valor inicial del Timer0
+    ;BTG     LATA,4,0          ; Invertir el estado del LED en RA3
+    RETFIE  FAST
+ 
+;********************************************************************************
+;GUARDAR NOTAS y DURACIONES
     
     
-RETARDO
-    MOVWF TEMP_DELAY  ; Usar el valor de TEMP como retardo
-LOOP_RETARDO
-    DECFSZ TEMP_DELAY,1
-    GOTO LOOP_RETARDO
+;NOTAS: Falta hacer el bucle que corra START_GAME hasta que se llegue a la cantidad guardada en CANTIDAD_NOTAS
+;********************************************************************************
+    
+GET_SEVEN_SEG ;ESTO NO SE USAAAAAAAAAA
+    ADDWF PCL, F          ; Sumar el índice al contador de programa
+    ; Salta al valor correspondiente en la tabla siguiente;
+SEVENSEG_TABLE
+    RETLW b'10000010'      ; 0
+    RETLW b'11111010'      ; 1
+    RETLW b'10010001'      ; 2
+    RETLW b'10000101'      ; 3
+    RETLW b'11001100'      ; 4
+    RETLW b'10100100'      ; 5
+    RETLW b'10100000'      ; 6
+    RETLW b'10001111'      ; 7
+    RETLW b'10000000'      ; 8
+    RETLW b'10001100'      ; 9
+    RETURN                 ; Retornar a START_GAME
+    
+    
+INIT_START_GAME
+    ;Preparamos el puntero para las notas
+    BCF LATA,3,0
+    
+    MOVLW PUNTERO_7SEG
+    MOVWF FSR0L
+    
+    MOVLW SEVENSEGMENTS_CERO
+    MOVWF INDF0
+    INCF FSR0L,1,0
+    MOVLW SEVENSEGMENTS_UNO
+    MOVWF INDF0
+    INCF FSR0L,1,0
+    MOVLW SEVENSEGMENTS_DOS
+    MOVWF INDF0
+    INCF FSR0L,1,0
+    MOVLW SEVENSEGMENTS_TRES
+    MOVWF INDF0
+    INCF FSR0L,1,0
+    MOVLW SEVENSEGMENTS_CUATRO
+    MOVWF INDF0
+    INCF FSR0L,1,0
+    MOVLW SEVENSEGMENTS_CINCO
+    MOVWF INDF0
+    INCF FSR0L,1,0
+    MOVLW SEVENSEGMENTS_SEIS
+    MOVWF INDF0
+    INCF FSR0L,1,0
+    MOVLW SEVENSEGMENTS_SIETE
+    MOVWF INDF0
+    INCF FSR0L,1,0
+    
+    
+    MOVLW PRIMER_DATO ; Colocar el puntero en la primera nota
+    MOVWF FSR0L
+    
+    
+    ; Configurar PORTD como salida para el display de 7 segmentos
+    BCF     TRISD,0,0
+    BCF     TRISD,1,0
+    BCF     TRISD,2,0
+    BCF     TRISD,3,0
+    BCF     TRISD,4,0
+    BCF     TRISD,5,0
+    BCF     TRISD,6,0
+    BCF     TRISD,7,0
+    
+    MOVLW 0x0000
+    MOVWF FSR1H
+    
     RETURN
-;DISTANCIA NOTES
-INCREMENT_COUNTER
-    INCF TICS_COUNTER_L, F	; Incrementa comptador low
+
+UPDATE_7SEG
+
+    MOVF INDF0, W       ; Cargar el valor de la dirección apuntada por INDF0 al WREG
+    ANDLW 0x07          ; Enmascarar para obtener los 3 LSB (00000111)
+    ADDLW PUNTERO_7SEG        ; Sumar el valor de los 3 LSB + 0x0040
+    MOVWF FSR1L
+    
+    MOVF INDF1, W
+    MOVWF LATD,0
     RETURN
     
-RESET_COUNTER
-    CLRF TICS_COUNTER_L
+UPDATE_LENGTH
+    BCF LATA,3,0
+    BCF LATA,4,0
+    MOVF INDF0,W
+    ANDLW 0x0018
+    MOVWF TEMP
+    BTFSC TEMP,3,0
     BSF LATA,3,0
+    BTFSC TEMP,4,0
+    BSF LATA,4,0
     RETURN
     
-; --------------------------------------------
-; CONFIGURAR PUERTOS
-; --------------------------------------------
-INIT_PORTS
-    SETF    ADCON1
-    ;BSF     INTCON2, RBPU
-    ; Configurar RB3 (ECHO) como entrada, RB2 (TRIGGER) como salida
-    BSF TRISB,3,0
-    BCF TRISB,2,0
-    CLRF TRISD,0
-    CLRF LATD,0
-    CLRF TRISA,0
-    CLRF LATA,0
-  
-    ; Configurar RC5 como salida (Altavoz)
-    BCF TRISC,5,0
-    BCF PORTC,5,0
-    ;configurar TMR0
-    BCF RCON,IPEN,ACCESS ;Desactivem les prioritats
-    MOVLW b'10001000' ;Configurem el timer0 sin prescaler
-    MOVWF T0CON,ACCESS
-    BCF INTCON, TMR0IF, ACCESS	;Netejem
-    MOVLW HIGH(.64736)	    
-    MOVWF TMR0H,0
-    MOVLW LOW(.64736)	   
-    MOVWF TMR0L,0
-    BSF INTCON, GIE, ACCESS      ; Habilitar interrupciones globales
-    BSF INTCON, PEIE, ACCESS     ; Habilitar interrupciones periféricas
-    BSF INTCON, TMR0IE, ACCESS   ; Habilitar interrupción del Timer0
-    RETURN
-   
+STOP_PROGRAM
+    MOVLW 0x0000  ; Desactivar todas las interrupciones
+    MOVWF INTCON       ; Escribir la configuración en el registro
+    
+    BCF LATA,3,0
+    BCF LATA,4,0
+    
+    MOVLW SEVENSEGMENTS_GUION
+    MOVWF LATD
+    
+    LOOP
+	GOTO LOOP
+    GOTO STOP_PROGRAM
+
+START_GAME
+    
+    CALL INIT_START_GAME ;Prepara los puertos, las variables, los FSR, las interrupciones etc.
+    
+    ;INDF0 Apunta a la nota que hay que sacar por el 7seg
+    ;INDF1 Se usa en UPDATE_7SEG y apunta a el valor a sacar por el 7seg
+    CALL UPDATE_7SEG    ; Llamar a la rutina para obtener el valor
+    CALL UPDATE_LENGTH
+    INCF FSR0L,1,0 ;Pasamos a la siguiente nota y duracion
+    GOTO STOP_PROGRAM            
+GOTO START_GAME
+    
+GUARDAR_DATOS
+    
+    ;GUARDAR NOTA y DURACION (estan ambas en el puerto C)
+    MOVF PORTC, 0,0  ;Leemos los datos del puerto C
+    ANDLW 0x1F  ;Como del puerto C solo nos interesa Note3: RC0, Note2: RC1, Note0: RC2, Duration0: RC3, Duration1: RC4, multiplicamos por '00011111' para ignorar el resto de bits
+    MOVWF INDF0 ;Pasamos el valor a la memoria
+    
+    
+    INCF FSR0L,1,0
+    INCF CANTIDAD_NOTAS,1,0
 
     
-CONFIG_OSC  
-    MOVLW b'01110000' ;8MHz, oscil·lador primari
-    MOVWF OSCCON, ACCESS  
-    MOVLW b'01000000' ;PLL (x4)
-    MOVWF OSCTUNE, ACCESS ;Oscil·lador intern a 32MHz (8MHz *4)
+    ESPERAR_NEWNOTE
+    BTFSS PORTA,5,0
     RETURN
+    BSF LATA,0,0 ; Mandamos el pulso de ACK
+    BCF LATA,0,0
+    GOTO ESPERAR_NEWNOTE
     
-; --------------------------------------------
-; PROGRAMA PRINCIPAL
-; --------------------------------------------
+MODO_GUARDAR_DATOS
+    BSF LATA,3,0 ;Esto es para debugar, ni caso
+    BCF LATA,3,0
+    BTFSC PORTA,5,0 ;Miramos si NewNote está activado
+    CALL GUARDAR_DATOS ;Esto se ejecuta SI NN ESTÁ ACTIVADO
+    BTFSC PORTA,1,0 ;Miramos si StartGame está activado
+    GOTO START_GAME ;Esto se ejecuta SI SG ESTÁ ACTIVADO
+    GOTO MODO_GUARDAR_DATOS
+
 MAIN
-    CALL CONFIG_OSC
-    CALL INIT_PORTS
+    ; Configuración de los puertos
+    SETF    ADCON1            ; Configurar PORTA como digital
     
-LOOP
-    CALL ENVIAR_PULSO_10US
-    CALL MEDIR_DISTANCIA
-    CALL CALCULAR_NOTA
-    ;CALL GENERAR_SONIDO_RC5  ; Generar sonido en RC5 manualmente
+    CLRF    PORTA
+    CLRF    PORTB
+    CLRF    PORTC
+    CLRF    PORTD
+    
+    CLRF    LATA
+    CLRF    LATB
+    CLRF    LATC
+    CLRF    LATD
+    
+    SETF    TRISA,0		; Ponemos todo como entrada inicialmente, de esta forma, los puertos que no usemos quedarán como entradas.
+    SETF    TRISB,0
+    SETF    TRISC,0
+    SETF    TRISD,0
+    
+    BCF     TRISA,3,0           ; Configurar RA3 como salida (LED)
+    BCF     TRISA,4,0           ; Configurar RA4 como salida (LED)
+   
+    
+;Configuracion de los puertos
 
-    GOTO LOOP
+    ;Configurar RA3 y RA4 como salidas
+    BCF TRISA,3,0
+    BCF TRISA,4,0
+    BCF LATA,3,0 
+    BCF LATA,4,0
+    
+    ;Poner RC4 y RC3 como entradas (RC4: Duration1, RC3: Duration0)
+    BSF TRISC,4,0 ;****************
+    BSF TRISC,3,0 ;****************
+    
+    ;RA0: ACK como salida y RA1: StartGame como entrada
+    BCF TRISA,0,0 ;****************
+    BSF TRISA,1,0 ;****************
+    
+    ;RB5: NewNote como entrada
+    BSF TRISA,5,0 ;*************
+    
+    ;NOTE[3..0]: Note3: RC0, Note2: RC1, Note0: RC2 como entradas
+    BSF TRISC,0,0 ;*************
+    BSF TRISC,1,0 ;*************
+    BSF TRISC,2,0 ;*************
+    
+    CLRF CANTIDAD_NOTAS ;Poner a 0 la cantidad de notas
+    
+    ; DESACTIVAR cualquier interrupción (esto es temporal xq en este codigo todavia no necesitamos)
+    MOVLW b'00000000'
+    MOVWF INTCON
+    
+    ; COMENZAMOS
+    
+    ;Preparamos el puntero INDF0 para guardar las NOTAS y las DURACIONES
+    MOVLW 0x0000
+    MOVWF FSR0H
+    MOVLW 0x0000
+    MOVWF FSR0L
+    MOVLW PRIMER_DATO
+    MOVWF FSR0L
+    
+    ;Configuro una interrupcion para depuerar
+    MOVLW   b'01000111'       ; Configurar Timer0 en modo de 16 bits, prescaler 1:256
+    MOVWF   T0CON             ; Escribir la configuración del Timer0
+    MOVLW b'00100000'  ; Configuración del INTCON:
+                   ; Bit 7 (GIE/GIEH) = 0 -> Interrupciones globales deshabilitadas
+                   ; Bit 6 (PEIE/GIEL) = 0 -> Interrupciones de periféricos deshabilitadas
+                   ; Bit 5 (R0IE) = 0 -> Interrupción del Timer0 deshabilitada
+                   ; Bit 2 (TMR0IF) = 1 -> Flag del Timer0 habilitado (para consulta)
+    MOVWF INTCON       ; Escribir la configuración en el registro
 
-END
+	
+    GOTO MODO_GUARDAR_DATOS
+    
+    END
